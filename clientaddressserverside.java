c1e408b9d907f7867766a2a1700ce4b12f0ca3d6
ajayreddyg <108275447+ajayreddyg@users.noreply.github.com>
2022-09-26 19:12:02 +0530

    Update clientaddressserverside.java

--- clientaddressserverside.java
+++ clientaddressserverside.java
@@ -1,72 +1,43 @@
-Aim : Design a socket program to print the client address at the server side
-Algorithm
-Server:
-1. Import all the required io and net packages
-2. Create a class Sip
-3. Initialize the classes ServerSocket and Socket and their respective objects.
-4. Initialize the class DataInputStream for reading the data from socket through
-getInputStream() method.
-5. Establish the connection with client system using the client IP and port number 8020
-6. Read the IP address from the socket
-7. Print the IP address at the server
-Client:
-1. Import all the required io and net packages
-2. Create a class Cip
-3. Initialize the classes Socket and their respective objects.
-4. Initialize the class PrintStream Class for writing data into the socket through
-getOutputStream() method.
-5. Get the IP address of the system using InetAddress class with getLocalHost() method.
-6. Establish the connection with server system using the IP address and port number 8020
-7. Write the IP address into the socket
-8. If any exception arise, print the error.
-Program:
-import java.io.*;
-import java.net.*;
-class Sip
-{
-public static void main(String args[])
-{
-ServerSocket ss;
-Socket s;
-DataInputStream dis;
-String ip;
-try
-{
-ss=new ServerSocket(8020);
-while(true)
-{
-s=ss.accept();
-dis=new DataInputStream(s.getInputStream());
-ip=dis.readLine();
-System.out.println("Ip address of the client system is"+ip);
+Aim: Program to print client address at server side. 
+Algorithm: SERVER STEP1: Create instances for socket and ServerSocket class.STEP2: Use 
+the port 9000 for TCP.STEP3: Make the PrintStream object connect to the OuputStream 
+using Socket.STEP4: Create an instance of the Date class and write it into the Socket.STEP5: 
+Get the IP address of the client using the socket and getlnetAddress ().STEP6: Print the 
+client's IPAddress. 
+CLIENT STEPI: Create instances for socket class with the port number 9000.STEP2: Create 
+an object of DatalnputStream and make it to get data from server through the socket. 
+STEP3: Read the Date object.STEP4: Print the obtained date. 
+Program.: 
+Server: 
+import java.net.*, 
+import java.io.* 
+class server4 
+public static void main(String[] args) throws Exception 
+ServerSocket s1 = new ServerSocket(8000); 
+System.out.println("Server Running "); 
+Socket s2=s1.accept); 
+InetAddress a=InetAddress.getLocalHost): 
+String add=a.getHostAddress(); 
+BufferedReader in-new BufferedReader(new 
+InputStreamReader(s2.getlnputStream0); 
+PrintWriter out=new Print Writer(s2.getOutputStream),true); 
+System.out.println("Client Connected"); 
+System.out.println(s2): 
+out.println(add); 
+System.out.println("Client's IP is "): 
+System.outprintln(in.readLine0); 
+sl.close): 
+s2.close); 
+Client: 
+import java.net."; import java.io.*; 
+class client4 
+public static void main(Stringl] args) throws Exception 
+InetAddress a=InetAddress.getLocalHost): 
+Socket s2 new Socket(a, 8000); 
+String adda.getHostAddress); BufferedReader in=new BufferedReader (new 
+InputStreamReader(s2.getlnputStream)); 
+PrintWriter out=new Print Writer(new OutputStreamWriter(s2.getOutputStream(), true); 
+System.out.println(in.readLine 0); out.println(add); 
+s2.close); 
 }
 }
-catch(IOException e)
-{
-System.out.println("The exception is: "+e);
-}
-}
-}
-import java.io.*;
-import java.net.*;
-class Cip
-{
-public static void main(String args[])
-{
-Socket soc;
-PrintStream ps;
-try
-{
-InetAddress ia=InetAddress.getLocalHost();
-soc=new Socket(ia,8020);
-ps=new PrintStream(soc.getOutputStream());
-ps.println(ia);
-}
-catch(IOException e)
-{
-System.out.println("The exception is: "+e);
-}
-}
-}
-Output
-Server side Terminal
